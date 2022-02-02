6833042299608e474a085e7877e295caed3c30ea
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2022-02-02 08:33:10 +0700

    mythauth

--- app/Views/layouts/sidebar-admin.php
+++ app/Views/layouts/sidebar-admin.php
@@ -2,14 +2,14 @@
       <nav class="sb-sidenav accordion sb-sidenav-light bg-white" id="sidenavAccordion">
         <div class="sb-sidenav-menu">
           <div class="nav">
-            <div class="sb-sidenav-menu-heading">Core</div>
+            
             <a class="nav-link" href="<?= base_url('admin') ?>">
               <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
               Dashboard
             </a>
             <a class="nav-link collapsed" href="" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
               <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
-              Santri
+             Santri MI
               <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
             </a>
             <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
@@ -19,9 +19,22 @@
                 <a class="nav-link" href="<?= base_url('admin/sekolah-asal') ?>">Sekolah Asal</a>
               </nav>
             </div>
+            <a class="nav-link collapsed" href="" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+              <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+             Santri RA
+              <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+            </a>
+            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+              <nav class="sb-sidenav-menu-nested nav">
+                <a class="nav-link" href="<?= base_url('admin/') ?>"> Data Santri</a>
+                <a class="nav-link" href="<?= base_url('admin/') ?>">Orang Tua</a>
+                <a class="nav-link" href="<?= base_url('admin/') ?>">Sekolah Asal</a>
+              </nav>
+            </div>
+
             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
               <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-              Pages
+              Berkas
               <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
             </a>
             <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
